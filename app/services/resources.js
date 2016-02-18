9fa152f7f200f02b9cb8f21969af61e3d1d2dc2d
Bill Farmakis <billfarmakis@iinet.net.au>
2016-02-18 16:49:00 +1100

    Added progress bar and download buttons for files

--- app/services/resources.js
+++ app/services/resources.js
@@ -22,9 +22,41 @@
             return deferred.promise;
         }
 
+        this.getCsvUrl = function (pageId) {
+            var deferred = $q.defer();
+            var requestUrl = 'http://localhost:8080/api/page/csv/' + pageId;
+
+            $http.get(requestUrl
+            ).success(function (response) {
+                    deferred.resolve(response);
+                }).error(function (data, status, headers, config) {
+                    deferred.reject("Error retrieving csv url for page id = " + pageId);
+                });
+
+            return deferred.promise;
+        }
+
+        this.getImageUrl = function (pageId) {
+            var deferred = $q.defer();
+            var requestUrl = 'http://localhost:8080/api/page/image/' + pageId;
+
+            $http.get(requestUrl
+            ).success(function (response) {
+                    deferred.resolve(response);
+                }).error(function (data, status, headers, config) {
+                    deferred.reject("Error retrieving image url for page id = " + pageId);
+                });
+
+            return deferred.promise;
+        }
+
         this.save = function (page) {
             var deferred = $q.defer();
             var formData = new FormData();
+
+            if (page.pageId) {
+                formData.append('pageId', page.pageId);
+            }
 
             if (page.chapterId) {
                 formData.append('chapterId', page.chapterId);
@@ -46,7 +78,7 @@
                 formData.append('csvFile', page.csvFile);
             }
 
-            $http.post('http://localhost:8080/api/page/create', formData, {
+            $http.post('http://localhost:8080/api/page/save', formData, {
                 transformRequest: angular.identity,
                 headers: {'Content-Type': undefined}
             }).success(function (response) {
