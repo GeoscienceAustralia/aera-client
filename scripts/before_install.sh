a6ba6c87de1f4259354a835d862da693cc0445da
Erin Zimmer <erin.zimmer@auspost.com.au>
2016-03-07 14:31:28 +1100

    Clean out old stuff before trying to get new stuff

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 aws s3 cp s3://smallest-bucket-in-history/aera-client/ /tmp --recursive --exclude "*" --include "*.zip"
+rm -rf /tmp/aera
 mkdir /tmp/aera
 cd /tmp
 unzip aera.zip -d aera
